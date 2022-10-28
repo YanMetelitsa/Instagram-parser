774d3f135e4d3c3617ecc6999381ab526aacfb7c
Yan Metelitsa <Yan.Metelitsa@gmail.com>
2022-10-28 10:57:00 +0300

    Release update
    ## Features
    
    - Followers parsing limit (default Infinity)
    - Format download file output data
    
    - Author data
    - Console output style
    - Every code block has comments
    - Download file name and extension change

--- parser.js
+++ parser.js
@@ -1,74 +1,115 @@
-class YMParser {
-	static version = '0.0.1';
+const app = {
+	name:       'YMInstagramParser',
+	author:     'Yan Metelitsa',
+	authorLink: 'https://yanmet.com/',
+	version:    '1.0.0',
 
-	/**
-	 * Create Parser
-	 */
-	constructor ( args ) {
-		this.options = args;
+	style:      'background-color: #7C04FC; padding: 4px 8px;',
+};
 
-		this.elements = {
-			usersListBox: document.querySelector( this.options.usersListBoxTag ),
-			usersListScrollBox: document.querySelector( this.options.usersListScrollBoxTag ),
-		};
-		this.usersList = [];
+const settings = {
+	scrollDelay:               1000,
+	theSameHeightNum:          1,
+	followersLimit:            Infinity,
 
+	followersListBoxTag:       '._aano',
+	followersListScrollBoxTag: '._aano > div',
+
+	followerElementTag:        '[ aria-labelledby ]',
+	followerNameTag:           'div > span > a > span > div',
+	followerLinkTag:           'div > span > a',
+
+	checkLoadingTag:           '._aanq',
+
+	removeSymbols:             [],
+
+	download:                  true,
+	outputFormat:              `{$username} https://instagram.com{$link}\n`,
+};
+
+class YMInstagramParser {
+	/** Create Parser */
+	constructor ( params ) {
+		/** Get the start time of the script */
 		this.startTime = new Date();
+		
+		/** Load params in options */
+		this.options = params;
+
+		/** Get main script elements */
+		this.elements = {
+			followersListBox: document.querySelector( this.options.followersListBoxTag ),
+			followersListScrollBox: document.querySelector( this.options.followersListScrollBoxTag ),
+		};
+		this.followersList = [];
 
 		console.clear();
-		console.log( `YM Parser v${YMParser.version} started with settings:` );
-		console.table( args );
+		console.log( `%c${app.name} v${app.version}`, app.style, `started with settings:` );
+		console.table( params );
+		console.log( `%cDevelopment`, app.style, `${app.author} ${app.authorLink}` );
 
-		console.log( `Press OK in instagram window to start parsing` );
+		console.log( `Click OK in the Instagram tab to start parsing` );
 
-		if ( window.confirm( 'Press OK to start parsing' ) ) {
-			this.getUsersList();
+		/** Press OK to start */
+		if ( window.confirm( 'Click OK to start parsing' ) ) {
+			this.getFollowersList();
 		} else {
-			console.log( `YMParser stopped by user` );
+			console.warn( `${app.name} stopped by user` );
 		}
 	}
 
-	/**
-	 * Get users
-	 */
-	getUsersList () {
+	/** Get followers list */
+	getFollowersList () {
 		let iterations = 0;
 
-		let lastUsersListScrollBoxHeight = 0;
-		let theSameHeightIterations = 0;
+		let lastFollowersListScrollBoxHeight = 0;
+		let theSameHeightIterationsNum = 0;
 
 		console.log( `Scroll interval started. Please wait...` );
 
+		/** Scroll interval */
 		const interval = setInterval( () => {
+			/** Check every 2 times if last scroll box had the same height with current */
 			if ( iterations % 2 == 0 ) {
-				if ( lastUsersListScrollBoxHeight == this.usersListScrollBoxHeight ) {
-					theSameHeightIterations++;
+				if ( lastFollowersListScrollBoxHeight == this.followersListScrollBoxHeight ) {
+					theSameHeightIterationsNum++;
 				} else {
-					theSameHeightIterations = 0;
+					theSameHeightIterationsNum = 0;
 				}
 
-				lastUsersListScrollBoxHeight = this.usersListScrollBoxHeight;
+				lastFollowersListScrollBoxHeight = this.followersListScrollBoxHeight;
 			}
 
-			this.usersListScroll();
+			/** Scroll */
+			this.followersListScroll();
 
 			iterations++;
 
-			console.clear();
-			console.log( iterations, `Users parsed: ${this.usersListBoxElements.length}` );
+			const parsingTime = Math.abs( new Date() - this.startTime ) / 1000;
+			const followersParsed = this.followersListBoxElements.length;
 
-			if ( theSameHeightIterations >= this.options.theSameHeightNum ) {
+			console.clear();
+			console.log( iterations, `Followers parsed: ${followersParsed} / ${this.options.followersLimit} in ${parsingTime} seconds` );
+
+			/** Check if scroll stopped */
+			if ( theSameHeightIterationsNum >= this.options.theSameHeightNum || this.options.followersLimit < followersParsed ) {
 				clearInterval( interval );
 
-				let usersElementsList = this.usersListBoxElements;
-				usersElementsList.forEach( userElement => {
-					this.pushUserData( userElement );
+				/** Push data in main followers array */
+				this.followersListBoxElements.forEach( ( followerElement, index ) => {
+					if ( index <= this.options.followersLimit ) {
+						this.pushFollowerData( followerElement );
+					} else {
+						return;
+					}
 				});
 
-				console.log( `Scroll interval successfully stopped and got ${usersElementsList.length} users` );
+				console.log( `Scroll interval successfully stopped and got ${followersParsed} followers` );
 
+				/** Show data */
 				this.showData();
 
+				/** Download data */
 				if ( this.options.download ) {
 					this.downloadData();
 				}
@@ -76,90 +117,88 @@ class YMParser {
 		}, this.options.scrollDelay );
 	}
 
+	/**  Scroll followers list */
+	followersListScroll () {
+		this.elements.followersListBox.scroll( 0, this.followersListScrollBoxHeight );
+	}
+
 	/**
-	 * Push user data into main users array
+	 * Push follower data in main followers array
+	 * 
+	 * @param {Element} followerElement Followers list element
 	 */
-	pushUserData ( userElement ) {
+	pushFollowerData ( followerElement ) {
+		/** Get data */
 		let output = {
-			username: userElement.querySelector( this.options.userNameTag ).innerHTML,
-			link: userElement.querySelector( this.options.userLinkTag ).getAttribute( 'href' ),
+			username: followerElement.querySelector( this.options.followerNameTag ).innerText,
+			link:     followerElement.querySelector( this.options.followerLinkTag ).getAttribute( 'href' ),
 		};
 		
+		/** Remove symbols from output */
 		this.options.removeSymbols.forEach( symbol => {
 			Object.keys( output ).forEach( key => {
 				output[ key ] = output[ key ].replaceAll( symbol, '' );
 			});
 		});
 
-		this.usersList.push( output );
+		this.followersList.push( output );
 	}
 
-	/**
-	 * Scroll users list
-	 */
-	usersListScroll () {
-		this.elements.usersListBox.scroll( 0, this.usersListScrollBoxHeight );
-	}
-
-	/**
-	 * Print users data
-	 */
+	/** Print followers data */
 	showData () {
 		const parsingTime = Math.abs( new Date() - this.startTime ) / 1000;
 
-		console.table( this.usersList );
-		console.log( `Parsed ${this.usersList.length} users in ${parsingTime} seconds` );
+		console.clear();
+		console.table( this.followersList );
+		console.log( `Parsed ${this.followersList.length} followers in ${parsingTime} seconds` );
 	}
 
 	/**
-	 * Download data to txt file
+	 * Download data to file
+	 * 
+	 * @param {string} filename Name of the output file
+	 * @param {string} extension Extension of the output file
 	 */
-	downloadData () {
+	downloadData ( filename = app.name, extension = 'txt' ) {
+		/** Format data */
+		let data = ``;
+		this.followersList.forEach( follower => {
+			const tags = {
+				'{$username}': follower.username,
+				'{$link}':     follower.link,
+			};
+
+			let row = this.options.outputFormat;
+			for ( const tag in tags ) {
+				row = row.replaceAll( tag, tags[ tag ] );
+			}
+
+			data += row;
+		});
+
+		/** Create download link element */
 		const linkElement = document.createElement( 'a' );
-		const data = 'test';
-
 		linkElement.setAttribute( 'href', 'data:text/plain;charset=utf-8,' + encodeURIComponent( data ) );
-		linkElement.setAttribute( 'download', 'YMParser.txt' );
+		linkElement.setAttribute( 'download', `${filename}.${extension}` );
 
-		linkElement.style.display = 'none';
+		/** Download and remove link */
 		document.body.appendChild( linkElement );
-
 		linkElement.click();
-
 		document.body.removeChild( linkElement );
 
-		console.log( `File with parsed users downloaded` );
+		console.log( `File with parsed followers downloaded` );
 	}
 
-	/**
-	 * Get height of users list scroll box
-	 */
-	get usersListScrollBoxHeight () {
-		return this.elements.usersListScrollBox.offsetHeight;
+	/** Get height of followers list scroll box */
+	get followersListScrollBoxHeight () {
+		return this.elements.followersListScrollBox.offsetHeight;
 	}
 
-	/**
-	 * Get elements from users list box
-	 */
-	get usersListBoxElements () {
-		return this.elements.usersListBox.querySelectorAll( this.options.userElementTag );
+	/**  Get elements from followers list box */
+	get followersListBoxElements () {
+		return this.elements.followersListScrollBox.querySelectorAll( this.options.followerElementTag );
 	}
 }
 
-const parser = new YMParser({
-	scrollDelay:           1000,
-	theSameHeightNum:      1,
-
-	usersListBoxTag:       '._aano',
-	usersListScrollBoxTag: '._aano > div',
-
-	userElementTag:        '[ aria-labelledby ]',
-	userNameTag:           'div > span > a > span > div',
-	userLinkTag:           'div > span > a',
-
-	checkLoadingTag:       '._aanq',
-
-	removeSymbols:         [ `'` ],
-
-	download:              true,
-});
+/** Create parser and start */
+const parser = new YMInstagramParser( settings );
